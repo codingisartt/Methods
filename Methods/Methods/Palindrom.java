bd66ca327914d175d16d50e8d00e369c64a2576a
Tubanur Balci <40777037+codingisartt@users.noreply.github.com>
2021-12-05 22:33:29 +0300

    Create: Polindrom.java is added

---
+++ Methods/Methods/Palindrom.java
@@ -0,0 +1,29 @@
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+public class Palindrom{
+    static boolean isPalindrom(int number){
+        int temp=number, reverseNumber=0,lastNumber;
+        while(temp!=0){
+            //System.out.println("=================");
+            //System.out.println("Sayı=> " + temp);
+            lastNumber=temp % 10;
+            //System.out.println("Son basamak => " + lastNumber);
+            reverseNumber=(reverseNumber * 10) +lastNumber;
+            //System.out.println("Yeni Sayı=> " + reverseNumber);
+            temp /= 10;
+            //System.out.println("Yeni Temp =>"+temp);
+            
+        }
+        if(number == reverseNumber)
+            return true;
+        else
+            return false;
+        
+            
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(4));
+        
+        
+    }
+}
